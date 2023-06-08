655f308be28e51191d9c992f52716cb63fd9ca0f
Marcoski05 <marco.crivas24@bishopireton.org>
2023-06-07 22:05:55 -0400

    Set up and initial class

---
+++ Eight-Queens/src/ChessSquarePanel.java
@@ -0,0 +1,47 @@
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ */
+
+/**
+ * @author mcecc
+ *
+ */
+public class ChessSquarePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	public ChessSquarePanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 */
+	public ChessSquarePanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param isDoubleBuffered
+	 */
+	public ChessSquarePanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 * @param isDoubleBuffered
+	 */
+	public ChessSquarePanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
